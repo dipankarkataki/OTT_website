89a558ef9dc0bac061077dc3c0d2f3be0ca7e01a
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-06-17 12:31:49 +0530

    mobile responsive working on

--- src/pages/Home/FreeTrialBanner/FreeTrialBanner.js
+++ src/pages/Home/FreeTrialBanner/FreeTrialBanner.js
@@ -9,7 +9,7 @@ function FreeTrialBanner() {
   return (
     <div className='text-center mt-8 mb-8 free-trial-banner'>
         <Image src=".././assets/images/free-trial-banner.png" alt="Free trial banner" width="100%"/>
-        <Button className='free-trial-banner-btn hidden md:block' label='Start Free Trial'  severity='danger' raised  icon='pi pi-arrow-right' iconPos="right" />
+        <Button className='free-trial-banner-btn hidden md:block' label='Start Free Trial'  size="large" severity='danger' raised  />
     </div>
   )
 }

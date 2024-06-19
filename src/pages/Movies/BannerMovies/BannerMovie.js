5cd81636dba75d9d0b41d74ed100e5a482f24d22
dipankarkataki <dipankarkataki@gmail.com>
2024-06-19 23:27:33 +0530

    movie genre section

--- src/pages/Movies/BannerMovies/BannerMovie.js
+++ src/pages/Movies/BannerMovies/BannerMovie.js
@@ -41,26 +41,28 @@ function BannerMovie() {
     const movieTemplate = (movie) => {
         return (
            <div className='banner-movie'>
-                
                 <Image src=".././assets/images/2states.jpg" alt="Banner Movie" width="100%"/>
-                
+                <div className='movie-content'>
+                    <div className='text-center'>
+                        <h1 className='text-white name'>Heeramandi</h1>
+                    </div>
+                    <p className='text-white-alpha-80 description hidden'>
+                        Coming from two very different cultural backgrounds, Krish and Ananya try to convince
+                        their parents to bless their relationship before they get married.
+                    </p>
+                    <div className='flex flex-row justify-content-center mt-3'>
+                        <Button label="Play Now" severity='danger' className='font-bold' raised/>
+                    </div>
+                </div>
            </div>
         );
     };
     return (
         <div className="card overflow-hidden banner-movie-card">
-            <div className='overlay'></div>
+            {/* <div className='overlay'></div> */}
             {/* <Carousel value={movie} responsiveOptions={responsiveOptions} circular autoplayInterval={3000} itemTemplate={movieTemplate} /> */}
             <Carousel value={movie} responsiveOptions={responsiveOptions} itemTemplate={movieTemplate} />
-            {/* <div className='movie-content'>
-                <div className='text-center'>
-                    <h1 className='text-white name'>Heeramandi</h1>
-                </div>
-                <p className='text-white-alpha-80 description'>
-                    Coming from two very different cultural backgrounds, Krish and Ananya try to convince
-                    their parents to bless their relationship before they get married.
-                </p>
-            </div> */}
+            
         </div>
     )
 }
--- src/pages/Movies/List/MovieList.js
+++ src/pages/Movies/List/MovieList.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './MovieList.css';
-import { Fieldset } from 'primereact/fieldset';
+import CategoryCards from '../../CategoryCards/CategoryCards';
 
 function MovieList() {
   return (
-    <div className='movie-list ml-5 mr-5 mt-4'> 
-      <Fieldset legend="Movies">
-          <p className="m-0 text-white">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-      </Fieldset>
+    <div className='movie-list ml-5 md:ml-5 mr-0 md:mr-5 mt-8'> 
+        <h1 className='text-white font-bold text-xl md:text-4xl mb-6 ml-5'>Our Genres</h1>
+        <div className='flex flex-row flex-wrap md:flex-nowrap justify-content-between mt-5 pb-8 gap-3'>
+          <CategoryCards />
+        </div> 
     </div>
   )
 }
